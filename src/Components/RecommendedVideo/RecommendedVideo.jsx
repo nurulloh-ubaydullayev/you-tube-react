3d7df29f6c978b07664c3bdb476812b3577928ad
NurullohUbaydullayev <nurulloh.ubaydullayev2004@gmail.com>
2021-09-21 22:48:59 +0500

    Add NavLink to recommended videos in Home page

--- src/Components/RecommendedVideo/RecommendedVideo.jsx
+++ src/Components/RecommendedVideo/RecommendedVideo.jsx
@@ -1,7 +1,18 @@
 import "./RecommendedVideo.scss";
+import { NavLink } from "react-router-dom";
 
 function RecommendedVideo({ children }) {
-  return <li className="recommended__item">{children}</li>;
+  return (
+    <li className="recommended__item">
+      <NavLink
+        className="recommended__item-link"
+        to="video"
+        style={{ textDecoration: "none" }}
+      >
+        {children}
+      </NavLink>
+    </li>
+  );
 }
 
 export default RecommendedVideo;
